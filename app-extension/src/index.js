e0a4d1e8aa668df288656c13daafc4f43fd5d820
Casimiro Rocha <contato@crsistemas.dev.br>
2024-12-29 02:48:13 -0300

    Initialize the project 🚀

---
+++ app-extension/src/index.js
@@ -0,0 +1,48 @@
+/**
+ * Quasar App Extension index/runner script
+ * (runs on each dev/build)
+ *
+ * Docs: https://quasar.dev/app-extensions/development-guide/index-api
+ */
+
+function extendConf (conf, api) {
+  // register our boot file
+  conf.boot.push('~quasar-app-extension-daddybrasil/src/boot/register.js')
+
+  if (api.hasWebpack) {
+    // make sure app extension files & ui package gets transpiled
+    const transpileTarget = (
+      conf.build.webpackTranspileDependencies // q/app-webpack >= v4
+      || conf.build.transpileDependencies // q/app-webpack v3
+    )
+    transpileTarget.push(/quasar-app-extension-daddybrasil[\\/]src/)
+  }
+
+  // make sure the stylesheet goes through webpack to avoid SSR issues
+  conf.css.push('~quasar-ui-daddybrasil/src/index.sass')
+}
+
+export default function (api) {
+  // Quasar compatibility check; you may need
+  // hard dependencies, as in a minimum version of the "quasar"
+  // package or a minimum version of "@quasar/app-*" CLI
+  api.compatibleWith('quasar', '^2.0.0')
+
+  if (api.hasVite) {
+    api.compatibleWith('@quasar/app-vite', '^1.5.0 || ^2.0.0')
+  }
+  else if (api.hasWebpack) {
+    api.compatibleWith('@quasar/app-webpack', '^3.10.0 || ^4.0.0')
+  }
+
+
+  // Uncomment the line below if you provide a JSON API for your component
+  // api.registerDescribeApi('MyComponent', '~quasar-ui-daddybrasil/src/components/MyComponent.json')
+
+  // Uncomment the line below if you provide a JSON API for your directive
+  // api.registerDescribeApi('my-directive', '~quasar-ui-daddybrasil/src/directives/my-directive.json')
+
+
+  // We extend /quasar.conf.js
+  api.extendQuasarConf(extendConf)
+}
